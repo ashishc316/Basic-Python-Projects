ac61722073837f718a8698e5679ad528b3768a50
ashishc316 <46437952+ashishc316@users.noreply.github.com>
2019-01-08 19:43:25 -0500

    Add files via upload

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -12,9 +12,22 @@ def dif(num1, num2):
 
 while match == True:
     gen = rm.randint(1, 10)
-    ans = int(input("Guess the number between 1 and 100 inclusive: "))
-    dif(ans,gen)
-    if ans == gen:
-        print("Congratulations!! You guessed the correct number!")
-        match = False
+    try:
+        ans = int(input("Guess the number between 1 and 10 inclusive: "))
+        if ans in [1,10]:
+            dif(ans, gen)
+            if ans == gen:
+                print("Congratulations!! You guessed the correct number!")
+                match = False
+        else:
+            print('Input value is beyond the acceptable limits')
+
+    except ValueError:
+        print("Invalid Input")
+    except ans > 100:
+        print("Input is greater than 100")
+
+
+
+
 
